2baa1a2fc16cf92046bcc8c45694f01592dea8f6
Allyson de Oliveira Brandão <allysomoliveira7@gmail.com>
2023-07-31 20:51:03 -0300

    Criado estiliização da land page

--- src/App.js
+++ src/App.js
@@ -1,28 +1,33 @@
 
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route , Link} from 'react-router-dom';
 
 import Home from './components/pages/Home';
 import Company from './components/pages/Company';
 import NewProject from './components/pages/NewProject';
+import Container from './components/layout/Container';
 
 function App() {
   return (
     <Router>
-      <ul>
-        <li>Home</li>
-        <li>Contato</li>
-      </ul>
+      <div>
+        <Link to="/">Home</Link>
+        <Link to="/contact">Contato</Link>
+        <Link to="/company">Empresa</Link>
+        <Link to="newProject">Novo Projeto</Link>
+      </div>
       <Switch>
-        <Route exact path='/'>
-          <Home/>
-        </Route>
-        <Route path="/company">
-          <Company/>
-        </Route>
-        <Route path="/newProject">
-          <NewProject/>
-        </Route>
+        <Container customClass="min-height">
+          <Route exact path='/'>
+            <Home/>
+          </Route>
+          <Route path="/company">
+            <Company/>
+          </Route>
+          <Route path="/newProject">
+            <NewProject/>
+          </Route>
+        </Container>
       </Switch>
       <p>Footer</p>
     </Router>
