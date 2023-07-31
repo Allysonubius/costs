4db5e6282c9a48332c5fe78387f65a4e8ef24afc
Allyson de Oliveira Brandão <allysomoliveira7@gmail.com>
2023-07-31 20:34:48 -0300

    Criado components da land page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
 
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+import Home from './components/pages/Home';
+import Company from './components/pages/Company';
+import NewProject from './components/pages/NewProject';
+
 function App() {
   return (
-    <div className="App">
-      <p>Costs</p>
-    </div>
+    <Router>
+      <ul>
+        <li>Home</li>
+        <li>Contato</li>
+      </ul>
+      <Switch>
+        <Route exact path='/'>
+          <Home/>
+        </Route>
+        <Route path="/company">
+          <Company/>
+        </Route>
+        <Route path="/newProject">
+          <NewProject/>
+        </Route>
+      </Switch>
+      <p>Footer</p>
+    </Router>
   );
 }
 
